31c37e61b73979acc8a780a5249aa3ba251d3def
conatus-recedendi <kylee@student.42seoul.kr>
2021-08-17 03:52:49 +0900

    feat: Add Login and list of shop

---
+++ flutter/lib/models/user.dart
@@ -0,0 +1,18 @@
+import 'dart:core';
+
+class UserMe {
+  UserMe({
+    // required this.email,
+    // required this.name,
+    // required this.nickname,
+    // required this.role,
+    // required this.phoneNumber,
+    required this.jwt,
+  });
+  // final String email;
+  // final String name;
+  // final String nickname;
+  // final String role;
+  // final String phoneNumber;
+  final String jwt;
+}
