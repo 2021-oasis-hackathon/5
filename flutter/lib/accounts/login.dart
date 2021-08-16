81a724a1b9744def2144a2bfd4a9f2e3db2433bf
conatus-recedendi <kylee@student.42seoul.kr>
2021-08-17 00:50:07 +0900

    feat: Add Login API

--- flutter/lib/accounts/login.dart
+++ flutter/lib/accounts/login.dart
@@ -1,41 +1,50 @@
+import 'dart:convert';
 import 'dart:io';
-import 'package:http/http.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:qount/models/user.dart';
+import 'package:qount/utils/display.dart';
+import '../main.dart';
 import 'Create_account.dart';
 import 'package:qount/examples/s_panel_example.dart';
 import '../screens/home/slidingup_panel.dart';
-void main() => runApp(login_home());
 
-class login_home extends StatelessWidget {
-  const login_home({Key? key}) : super(key: key);
+void main() => runApp(Login());
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: login(),
+      home: LoginHome(),
     );
   }
 }
 
-class login extends StatefulWidget {
-  const login({Key? key}) : super(key: key);
+class LoginHome extends StatefulWidget {
+  const LoginHome({Key? key}) : super(key: key);
 
   @override
-  _loginState createState() => _loginState();
+  _LoginState createState() => _LoginState();
 }
 
-class _loginState extends State<login> {
+class _LoginState extends State<LoginHome> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 100,),
-
+            SizedBox(
+              height: 100,
+            ),
             Padding(
               padding: const EdgeInsets.only(top: 60.0),
               child: Center(
@@ -48,36 +57,41 @@ class _loginState extends State<login> {
                     child: Image.asset('imgs/logo.png')),
               ),
             ),
-            SizedBox(height: 100,),
+            SizedBox(
+              height: 100,
+            ),
             Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: EdgeInsets.symmetric(horizontal: 60),
               child: TextField(
                 decoration: InputDecoration(
-                    fillColor: Colors.white10,
-                    filled: true,
-                    border: OutlineInputBorder(),
-                    labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com'),
+                  fillColor: Colors.white10,
+                  filled: true,
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Enter valid email id as abc@gmail.com',
+                ),
+                controller: _emailController,
               ),
             ),
-            SizedBox(height: 15,),
+            SizedBox(
+              height: 15,
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 60),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
-
                 obscureText: true,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter secure password',
+                ),
+                controller: _passwordController,
               ),
             ),
-            FlatButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
+            TextButton(
+              onPressed: () {},
               child: Text(
                 'Forgot Password',
                 style: TextStyle(color: Colors.blue, fontSize: 15),
@@ -88,9 +102,22 @@ class _loginState extends State<login> {
               width: 250,
               decoration: BoxDecoration(
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: FlatButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => sliding_home()));
+              child: TextButton(
+                onPressed: () async {
+                  var email = _emailController.text;
+                  var password = _passwordController.text;
+                  var user = await attemptLogIn(email, password);
+                  if (user.jwt != "") {
+                    SchedulerBinding.instance!
+                        .addPostFrameCallback((timeStamp) {
+                      // user as argumnet
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => sliding_home()));
+                    });
+                  } else {
+                    displayDialog(context, "An Error Occurred",
+                        "No Account was found matching that username and password");
+                  }
                 },
                 child: Text(
                   'Login',
@@ -105,12 +132,70 @@ class _loginState extends State<login> {
                 onPressed: () {
                   Navigator.push(
                       context, MaterialPageRoute(builder: (_) => SignUp()));
-                }
-                ,child: Text('New User? Create Account')
-            )
+                },
+                child: Text('New User? Create Account'))
           ],
         ),
       ),
     );
   }
+
+  Future<UserMe> attemptLogIn(String email, String password) async {
+    var res = await http.post(
+      Uri.parse("$SERVER_IP/login"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(
+        {
+          'authentication': {
+            "email": email,
+            "password": password,
+          }
+        },
+      ),
+    );
+    if (res.statusCode == 200) {
+      UserMe me = UserMe(jwt: json.decode(res.body)['auth_token']);
+      return me;
+    }
+    UserMe me = UserMe(jwt: "");
+    return me;
+  }
+
+  Future<int> attemptSignUp(
+      String email,
+      String password,
+      String name,
+      String passwordConfirmation,
+      String role,
+      String phoneNumber,
+      String nickname) async {
+    var res = await http.post(
+      Uri.parse('$SERVER_IP/users'),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(
+        {
+          "user": {
+            "name": name,
+            "email": email,
+            "password": password,
+            "password_confirmation": passwordConfirmation,
+            "role": role,
+            "phone_number": phoneNumber,
+            "nickname": nickname
+          }
+        },
+      ),
+    );
+
+    return res.statusCode;
+  }
 }
+    // "user":{
+    // "name":"kylee_customer",
+    // "email": "email1@email.com",
+    // "nickname": "나는밥버거",
+    // "password": "pwd",
+    // "password_confirmation": "pwd",
+    // "role": "customer",
+    // "phone_number": "010-1234-1234"
+    // }
